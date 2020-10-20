c275cbd71118976d65de3e0bddbd3811458738d5
ManuXu <61727227+xuyihan-china@users.noreply.github.com>
2020-10-20 22:16:18 +0800

    00

---
+++ Untitled4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+ char letter;
+ printf("piease enter the first number of someday:");
+ scanf("%c",&letter);
+ switch (letter){
+    case'M':
+    case'm':
+    
+       printf("Monday");
+       break;
+    case's':
+    case'S':
+	   printf("please enter the second number:");
+	   scanf("%c",&letter);
+	   scanf("%c",&letter);
+	   
+	   
+	   if(letter =='a')
+		  printf("saturday\n");
+		  else if (letter == 'u');
+		  printf("sunday\n");
+		  
+		  break;
+   }
+   return 0;
+}
+       
+    
+    
+
+
+
+
+
